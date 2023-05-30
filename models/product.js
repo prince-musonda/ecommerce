34c8086148d210ac8a6310a9ef82a02163c41eac
Prince Musonda <115486288+prince-musonda@users.noreply.github.com>
2023-05-30 13:28:08 +0200

    code refactor

--- models/product.js
+++ models/product.js
@@ -3,33 +3,35 @@ const path = require('path')
 const rootDir = require('../utils/path')
 
 const p = path.join(rootDir,'data','product.js')
+
+function getProductsFromFile(callback){
+    fs.readFile(p,(error,fileContent)=>{
+        let products = [] // in case error in finding or opening file 
+        if(error){
+            callback(products)
+        }else{
+            products = JSON.parse(fileContent);
+            callback(products)
+        }
+        
+   })
+}
+
 class Product{
     constructor(title){
         this.title = title
     }
     save(){
-       fs.readFile(p,(error,fileContent)=>{
-            let products = []
-            if(!error){
-                products = JSON.parse(fileContent)
-            }
+       getProductsFromFile(products => {
             products.push(this)
-            fs.writeFile(p,JSON.stringify(products),error=>{
-                console.log(error)
+            fs.writeFile(p,JSON.stringify(products),(error)=>{
+                if(error) console.log(error)
             })
        })
     }
+
     static fetchAll(callback){
-        fs.readFile(p,(error,fileContent)=>{
-            let products = []
-            if(error){
-                callback(products)
-            }else{
-                products = JSON.parse(fileContent);
-                callback(products)
-            }
-            
-       })
+        getProductsFromFile(callback)
     }
 }
 
