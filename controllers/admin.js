2c125ddc77c07aa69cd088c6436832c51fa26d17
Prince Musonda <115486288+prince-musonda@users.noreply.github.com>
2023-06-01 13:56:40 +0200

    edited admin controllers edited admin controllers in order to include features of not  just adding new products but also updating existing products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,12 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    edit: false // to differentiate this contoller and the 
+    // getEditProduct since they are both rendering the same view.
+    // in getEditProduct, the edit property is sent to true
   });
 };
 
@@ -15,10 +15,45 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null,title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit
+  if(!editMode){
+    return res.redirect('/')
+  }
+  const productId = req.params.productId
+  Product.findById(productId,product =>{
+    if(!product){
+     return res.redirect('/')
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      product: product,
+      edit: editMode  // to differentiate this contoller and the 
+      // getAddProduct since they are both rendering the same view.
+      // in getAddProduct, the edit property is sent to false
+    });
+  })
+ 
+};
+
+
+exports.postEditProduct = (req,res,next) =>{
+  const productId = req.body.productId
+  const updatedTitle = req.body.title
+  const updatedPrice = req.body.price
+  const updatedDescription = req.body.description
+  const updatedImageUrl = req.body.imageUrl
+  const updatedProduct = new Product(productId,updatedTitle,updatedImageUrl,updatedDescription,updatedPrice)
+  updatedProduct.save()
+  res.redirect('/admin/products')
+
+}
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
