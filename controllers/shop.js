d7c3f739f3f42bed977aca687e66c6611c423aa1
Prince Musonda <115486288+prince-musonda@users.noreply.github.com>
2023-06-03 12:20:41 +0200

    added controller for handling delete requests of cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -37,12 +37,15 @@ exports.getCart = (req, res, next) => {
   Cart.getCart(cart => {
     Product.fetchAll(products =>{
       const cartProducts = []
-      for(let product of products){
-        const cartProductData = cart.products.find(p => p.id == product.id)
-        if(cartProductData){
-            cartProducts.push({productData: product, qty: cartProductData.qty})
-          }
+      if(cart != null){
+        for(let product of products){
+          const cartProductData = cart.products.find(p => p.id == product.id)
+          if(cartProductData){
+              cartProducts.push({productData: product, qty: cartProductData.qty})
+            }
         }
+      }
+      
         res.render('shop/cart', {
           path: '/cart',
           pageTitle: 'Your Cart',
@@ -62,6 +65,13 @@ exports.postCart = (req,res,next) =>{
   res.redirect('/cart')
 }
 
+exports.postCartDeleteProduct = (req,res,next) =>{
+  const productId = req.body.productId
+  Product.findById(productId, (product) =>{
+      Cart.deleteProduct(productId, product.price)
+      res.redirect('/cart')
+  })
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
